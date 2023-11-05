e00cebf3dc2780cc520a8bfdbd9532e029b4743c
DesC0DE <jaden.humphries@gmail.com>
2023-11-05 17:55:59 -0500

    Added RemoveDuplicateNodes.txt/.cpp

---
+++ code/Easy/LinkedList/RemoveDuplicateNodes.cpp
@@ -0,0 +1,27 @@
+using namespace std;
+// This is an input struct. Do not edit.
+class LinkedList{
+ public:
+  int value;
+  LinkedList* next = nullptr;
+
+  LinkedList(int value){ this->value = value; }
+};
+
+LinkedList* removeDuplicatesFromLinkedList(LinkedList* linkedList){
+  LinkedList* curr = linkedList;
+  
+  while(curr && curr->next){
+    
+    if(curr->value == curr->next->value){
+      LinkedList* nodeToDel = curr->next;
+      curr->next = curr->next->next;
+      delete nodeToDel;
+      
+    }else{
+    curr = curr->next;
+    }
+  }
+
+  return linkedList;
+}
